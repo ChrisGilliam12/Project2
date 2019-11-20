e4538fc6c0b4d07a5c53515bd3ea1d8e5fc1523c
Chris Gilliam <chrisgilliam123@yahoo.com>
2019-11-19 20:04:13 -0600

    removed Jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
-if(process.env.JAWSDB_URL) {
-  const connection =mysql.createConnection(process.env.JAWSDB_URL):
-} else {
+
   const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -12,7 +10,6 @@ if(process.env.JAWSDB_URL) {
   database: "baseballdb"
 })
 
-}
 
 ;
 
